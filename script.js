8bfbb188db85805700e02d272ba32f574a238898
Pranav.S <santhoshpranav2006@gmail.com>
2025-03-31 11:16:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showsidenav(){
+        const sidenav=document.querySelector(".sidenav");
+        sidenav.style.display='flex';
+    }
+    function hidesidenav(){
+        const sidenav=document.querySelector(".sidenav");
+        sidenav.style.display="none";
+    }
